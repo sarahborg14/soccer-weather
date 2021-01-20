bc392a1d41e3b60ad20d3254ecefa6ad2498cc94
Sarah Borg <borgsarah14@gmail.com>
2021-01-19 22:20:24 -0600

    Commits for Jan 19

--- ERA5_data_retrieval.py
+++ ERA5_data_retrieval.py
@@ -42,17 +42,18 @@ import numpy as np
 
 #32.982675029015795, -96.75066793061926 #exact 
 import cdsapi
-c = cdsapi.Client()
-c.retrieve('reanalysis-era5-complete', {
+cds = cdsapi.Client()
+cds.retrieve('reanalysis-era5-complete', 
+{
     'class': 'ea',
-    'date': '2018-09-01/2018-09-30/', #I think I can connect dates in this way? 
+    'date': '2018-09-01/2018-09-05', #I think I can connect dates in this way? 
     'expver': '1',
     'levtype': 'sfc',
     'param': '134.128/165.128/166.128/167.128/168.128',
-    'area': '33/-96.75',
-#     'grid': '0.3/0.3'
+	"area": "32.5/-97/33/-96.5",
+	"grid": "0.25/0.25",
     'stream': 'oper',
     'time': '00:00:00/01:00:00/02:00:00/03:00:00/04:00:00/05:00:00/06:00:00/07:00:00/08:00:00/09:00:00/10:00:00/11:00:00/12:00:00/13:00:00/14:00:00/15:00:00/16:00:00/17:00:00/18:00:00/19:00:00/20:00:00/21:00:00/22:00:00/23:00:00',
     'type': 'an',
-    'format': 'netcdf',
-}, 'UTD_sfc_data.nc')
+    'format': 'netcdf',   
+}, 'sfc_data.nc')
